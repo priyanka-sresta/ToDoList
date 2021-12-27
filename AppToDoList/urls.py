b2a61ed9588e94901f41804cb5ceab5cdb9a34f4
Priyanka <pnkasresta@gmail.com>
2021-12-27 15:14:06 +0545

    Make user login and logout pages
    User can only access and edit their own task lists.

--- AppToDoList/urls.py
+++ AppToDoList/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, UserLogin
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', UserLogin.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('create-task/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
 ]
